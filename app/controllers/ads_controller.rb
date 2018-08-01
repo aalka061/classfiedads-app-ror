c3846e8e7f5480b86b09d4ad301d05e0db03b627
aalka061 <a_algg@hotmail.com>
2018-08-01 01:01:20 -0400

    delete & edit func

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -6,4 +6,36 @@ class AdsController < ApplicationController
     def new 
         @ad = Ad.new
     end
+
+    def create
+        @ad = Ad.create(get_ad_params)
+        if @ad.save
+            flash[:success] = "Successfully created..."
+            redirect_to root_path
+        end
+
+    end
+    def edit
+        @ad = Ad.find(params[:id])
+
+    end
+
+    def update
+        @ad = Ad.find(params[:id])
+        @ad.update_attributes(get_ad_params)
+        redirect_to root_path
+
+
+    end
+    def destroy
+        @ad = Ad.find(params[:id])
+        @ad.destroy
+        redirect_to root_path
+    end
+
+    private 
+    def get_ad_params
+        params.require(:ad).permit(:title, :description, :location, :price)
+    end
 end
+
